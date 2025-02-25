5c71f48e8f4b810038107940ac1d1f84f3813958
christ48 <111949820+christ48@users.noreply.github.com>
2025-02-24 20:03:57 -0600

    Add files via upload

---
+++ administracionScoutsCR/Models/LoginViewmodel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace administracionScoutsCR.Models
+{
+    
+    public class LoginViewmodel
+    {
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public required string Correo { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; } = null!;
+    }
+}
