c285691af02eea1150361832634e69549896c916
Josué Alvarado <jalvarado670602@ufide.ac.cr>
2025-03-17 17:01:10 -0600

    Stashed
    a

--- administracionScoutsCR/Models/DatabaseScoutContext.cs
+++ administracionScoutsCR/Models/DatabaseScoutContext.cs
@@ -40,7 +40,11 @@ public partial class DatabaseScoutContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         => optionsBuilder.UseSqlServer("Server=LocalHost;Database=ScoutDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+=======
+        => optionsBuilder.UseSqlServer("Server=LocalHost;Database=Scoutdatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+>>>>>>> Stashed changes
 =======
         => optionsBuilder.UseSqlServer("Server=LocalHost;Database=Scoutdatabase;Trusted_Connection=True;TrustServerCertificate=True;");
 >>>>>>> Stashed changes
