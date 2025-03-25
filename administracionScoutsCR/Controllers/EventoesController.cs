f0390c5fd9e784b89809ed21d221cb61695631f2
Josué Alvarado <jalvarado670602@ufide.ac.cr>
2025-03-24 18:26:07 -0600

    Detalles de evento y confirmacion

--- administracionScoutsCR/Controllers/EventoesController.cs
+++ administracionScoutsCR/Controllers/EventoesController.cs
@@ -15,226 +15,268 @@ using System.Diagnostics.Metrics;
 
 namespace administracionScoutsCR.Controllers
 {
-	public class EventoesController : Controller
-	{
-		private readonly DatabaseScoutContext _context;
-		private readonly IConfiguration _configuration;
+    public class EventoesController : Controller
+    {
+        private readonly DatabaseScoutContext _context;
+        private readonly IConfiguration _configuration;
 
-		public EventoesController(DatabaseScoutContext context, IConfiguration configuration)
-		{
-			_context = context;
-			_configuration = configuration;
-		}
-		public async Task<IActionResult> Index()
-		{
-			return View(await _context.Eventos.ToListAsync());
-		}
-		public async Task<IActionResult> Calendar()
-		{
-			return View(await _context.Eventos.ToListAsync());
-		}
+        public EventoesController(DatabaseScoutContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            int? usuarioId = ObtenerUsuarioId();
 
-		public async Task<IActionResult> Test()
-		{
+            var eventos = await _context.Eventos
+                .Include(e => e.ConfirmacionEventos)
+                .ToListAsync();
 
-			return View(await _context.Eventos.ToListAsync());
-		}
-		// GET: Eventoes/Details/5
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+            // Contar confirmaciones por evento
+            ViewBag.Confirmaciones = eventos.ToDictionary(e => e.IdEvento, e => e.ConfirmacionEventos.Count);
 
-			var evento = await _context.Eventos
-				.FirstOrDefaultAsync(m => m.IdEvento == id);
-			if (evento == null)
-			{
-				return NotFound();
-			}
+            // Verificar si el usuario ha confirmado su asistencia
+            ViewBag.UsuarioConfirmaciones = usuarioId == null
+                ? new Dictionary<int, bool>()
+                : eventos.ToDictionary(
+                    e => e.IdEvento,
+                    e => e.ConfirmacionEventos.Any(c => c.IdUsuario == usuarioId.Value)
+                );
 
-			return View(evento);
-		}
+            return View(eventos);
+        }
+        public async Task<IActionResult> Calendar()
+        {
+            return View(await _context.Eventos.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-		// GET: Eventoes/Create
-		// GET: Eventoes1/Create
-		public IActionResult Create()
-		{
-			return View();
-		}
+            var evento = await _context.Eventos
+                .FirstOrDefaultAsync(e => e.IdEvento == id);
 
-		// POST: Eventoes1/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("IdEvento,Titulo,Fecha,Lugar,Descripcion,Encargado,ContactoEncargado")] Evento evento)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Add(evento);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			return View(evento);
-		}
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            // Obtener la lista de usuarios que confirmaron asistencia
+            var confirmedUsers = await _context.ConfirmacionEventos
+                .Include(c => c.IdUsuarioNavigation)
+                .Where(c => c.IdEvento == id)
+                .Select(c => c.IdUsuarioNavigation)
+                .ToListAsync();
+
+            ViewBag.ConfirmedUsers = confirmedUsers;
+
+            return View(evento);
+        }
 
 
-		// GET: Eventoes/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        // GET: Eventoes/Create
+        // GET: Eventoes1/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-			var evento = await _context.Eventos.FindAsync(id);
-			if (evento == null)
-			{
-				return NotFound();
-			}
-			return View(evento);
-		}
+        // POST: Eventoes1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdEvento,Titulo,Fecha,Lugar,Descripcion,Encargado,ContactoEncargado")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(evento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evento);
+        }
 
-		// POST: Eventoes/Edit/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("IdEvento,Titulo,Fecha,Lugar,Descripcion,Encargado,ContactoEncargado")] Evento evento)
-		{
-			if (id != evento.IdEvento)
-			{
-				return NotFound();
-			}
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(evento);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!EventoExists(evento.IdEvento))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			return View(evento);
-		}
+        // GET: Eventoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-		// GET: Eventoes/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+            return View(evento);
+        }
 
-			var evento = await _context.Eventos
-				.FirstOrDefaultAsync(m => m.IdEvento == id);
-			if (evento == null)
-			{
-				return NotFound();
-			}
+        // POST: Eventoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEvento,Titulo,Fecha,Lugar,Descripcion,Encargado,ContactoEncargado")] Evento evento)
+        {
+            if (id != evento.IdEvento)
+            {
+                return NotFound();
+            }
 
-			return View(evento);
-		}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(evento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventoExists(evento.IdEvento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evento);
+        }
 
-		// POST: Eventoes/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			var evento = await _context.Eventos.FindAsync(id);
-			if (evento != null)
-			{
-				_context.Eventos.Remove(evento);
-			}
+        // GET: Eventoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
+            var evento = await _context.Eventos
+                .FirstOrDefaultAsync(m => m.IdEvento == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
 
-		private bool EventoExists(int id)
-		{
-			return _context.Eventos.Any(e => e.IdEvento == id);
-		}
+            return View(evento);
+        }
 
-		// Método para verificar eventos próximos en 7 días
-		public async Task<IActionResult> VerificarEventosProximos()
-		{
-			var fechaLimite = DateTime.Now.AddDays(7);
-			var eventosProximos = await _context.Eventos
-				.Where(e => e.Fecha.Date == fechaLimite.Date)
-				.ToListAsync();
+        // POST: Eventoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento != null)
+            {
+                _context.Eventos.Remove(evento);
+            }
 
-			foreach (var evento in eventosProximos)
-			{
-				var usuariosConfirmados = await _context.ConfirmacionEventos
-					.Where(c => c.IdEvento == evento.IdEvento)
-					.Select(c => c.IdUsuarioNavigation.Correo)
-					.ToListAsync();
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
-				foreach (var correo in usuariosConfirmados)
-				{
-					await EnviarCorreoRecordatorio(correo, evento);
-				}
-			}
+        private bool EventoExists(int id)
+        {
+            return _context.Eventos.Any(e => e.IdEvento == id);
+        }
 
-			return Ok("Correos enviados para eventos próximos.");
-		}
+        // Método para verificar eventos próximos en 7 días
+        public async Task<IActionResult> VerificarEventosProximos()
+        {
+            var fechaLimite = DateTime.Now.AddDays(7);
+            var eventosProximos = await _context.Eventos
+                .Where(e => e.Fecha.Date == fechaLimite.Date)
+                .ToListAsync();
 
-		private async Task EnviarCorreoRecordatorio(string destinatario, Evento evento)
-		{
-			var apiKey = _configuration["SendGrid:ApiKey"];
-			var fromEmail = _configuration["SendGrid:FromEmail"];
+            foreach (var evento in eventosProximos)
+            {
+                var usuariosConfirmados = await _context.ConfirmacionEventos
+                    .Where(c => c.IdEvento == evento.IdEvento)
+                    .Select(c => c.IdUsuarioNavigation.Correo)
+                    .ToListAsync();
 
-			var client = new SendGridClient(apiKey);
-			var from = new EmailAddress(fromEmail, "ScoutsCR");
-			var subject = $"Recordatorio: {evento.Titulo} en 7 días";
-			var to = new EmailAddress(destinatario);
-			var plainTextContent = $"Hola, te recordamos que el evento '{evento.Titulo}' se llevará a cabo el {evento.Fecha}. ¡No faltes!";
-			var htmlContent = $"<strong>Hola,</strong><br><br>Te recordamos que el evento <b>{evento.Titulo}</b> se llevará a cabo el <b>{evento.Fecha}</b>.<br>¡No faltes!";
-			var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                foreach (var correo in usuariosConfirmados)
+                {
+                    await EnviarCorreoRecordatorio(correo, evento);
+                }
+            }
 
-			var response = await client.SendEmailAsync(msg);
-		}
+            return Ok("Correos enviados para eventos próximos.");
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> ConfirmarAsistencia(int idEvento)
-		{
-			var usuarioId = ObtenerUsuarioId();
+        private async Task EnviarCorreoRecordatorio(string destinatario, Evento evento)
+        {
+            var apiKey = _configuration["SendGrid:ApiKey"];
+            var fromEmail = _configuration["SendGrid:FromEmail"];
 
-			var confirmacionExistente = await _context.ConfirmacionEventos
-				.FirstOrDefaultAsync(c => c.IdEvento == idEvento && c.IdUsuario == usuarioId);
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(fromEmail, "ScoutsCR");
+            var subject = $"Recordatorio: {evento.Titulo} en 7 días";
+            var to = new EmailAddress(destinatario);
+            var plainTextContent = $"Hola, te recordamos que el evento '{evento.Titulo}' se llevará a cabo el {evento.Fecha}. ¡No faltes!";
+            var htmlContent = $"<strong>Hola,</strong><br><br>Te recordamos que el evento <b>{evento.Titulo}</b> se llevará a cabo el <b>{evento.Fecha}</b>.<br>¡No faltes!";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
-			if (confirmacionExistente == null)
-			{
-				var confirmacion = new ConfirmacionEvento
-				{
-					IdEvento = idEvento,
-					IdUsuario = usuarioId,
-					Asistencia = "Confirmado"
-				};
+            var response = await client.SendEmailAsync(msg);
+        }
 
-				_context.ConfirmacionEventos.Add(confirmacion);
-				await _context.SaveChangesAsync();
-			}
 
-			return RedirectToAction("Index");
-		}
+        [HttpPost]
+        public async Task<IActionResult> ConfirmarAsistencia(int idEvento)
+        {
+            int? usuarioId = ObtenerUsuarioId();
+            if (usuarioId == null)
+            {
+                return Json(new { success = false, message = "Usuario no autenticado." });
+            }
 
-		private int ObtenerUsuarioId()
+            var confirmacion = new ConfirmacionEvento
+            {
+                IdEvento = idEvento,
+                IdUsuario = usuarioId,
+                Asistencia = "Confirmado"
+            };
+
+            _context.ConfirmacionEventos.Add(confirmacion);
+            await _context.SaveChangesAsync();
+
+            int totalConfirmados = _context.ConfirmacionEventos.Count(c => c.IdEvento == idEvento);
+            return Json(new { success = true, confirmado = true, totalConfirmados });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EliminarConfirmacion(int idEvento)
+        {
+            int? usuarioId = ObtenerUsuarioId();
+            if (usuarioId == null)
+            {
+                return Json(new { success = false, message = "Usuario no autenticado." });
+            }
+
+            var confirmacion = await _context.ConfirmacionEventos
+                .FirstOrDefaultAsync(c => c.IdEvento == idEvento && c.IdUsuario == usuarioId);
+
+            if (confirmacion != null)
+            {
+                _context.ConfirmacionEventos.Remove(confirmacion);
+                await _context.SaveChangesAsync();
+            }
+
+            int totalConfirmados = _context.ConfirmacionEventos.Count(c => c.IdEvento == idEvento);
+            return Json(new { success = true, confirmado = false, totalConfirmados });
+        }
+        private int ObtenerUsuarioId()
         {
             return int.Parse(User.Claims.FirstOrDefault(c => c.Type == "IdUsuario")?.Value);
         }
