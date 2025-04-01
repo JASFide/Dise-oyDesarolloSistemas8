ad1c75269351d561d5be3f2e34cca5c1f50b32d2
Josué Alvarado <jalvarado670602@ufide.ac.cr>
2025-03-31 18:56:31 -0600

    Rol2

--- ScoutDatabase/dbo/Tables/Usuario.sql
+++ ScoutDatabase/dbo/Tables/Usuario.sql
@@ -11,7 +11,11 @@
     [Correo]          VARCHAR (50)  NOT NULL,
     [NumeroTelefono]  VARCHAR (50)  NOT NULL,
     [Contrasena]      VARCHAR (255) NOT NULL,
+    [IdRole]          INT           NULL,
     PRIMARY KEY CLUSTERED ([IdUsuario] ASC),
+    CONSTRAINT [FK_Usuario_Role] FOREIGN KEY ([IdRole]) REFERENCES [dbo].[Role] ([Id]),
     CONSTRAINT [FK_Usuario_Seccion] FOREIGN KEY ([IdSeccion]) REFERENCES [dbo].[Seccion] ([IdSeccion])
 );
+
+
 
