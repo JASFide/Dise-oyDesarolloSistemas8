530c47e65a4a94a611a9663ba3de4fefb28ec7c7
christ48 <christopheralvarez220302@gmail.com>
2025-04-13 21:40:32 -0600

    cambios visuales

--- administracionScoutsCR/Controllers/HomeController.cs
+++ administracionScoutsCR/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace administracionScoutsCR.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Historia()
+        {
+            return View();
+        }
+
     }
 }
