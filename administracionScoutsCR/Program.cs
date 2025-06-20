7a852d97f82c9bbb705d4fce1dfccd159aabcd52
Josué Alvarado <jalvarado670602@ufide.ac.cr>
2025-06-19 18:28:40 -0600

    Changes

--- administracionScoutsCR/Program.cs
+++ administracionScoutsCR/Program.cs
@@ -1,6 +1,7 @@
 using administracionScoutsCR.Controllers;
 using administracionScoutsCR.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DatabaseScoutContext>();
 builder.Services.AddHostedService<RecordatorioEventosService>();
-
+builder.Services.AddDbContext<DatabaseScoutContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
